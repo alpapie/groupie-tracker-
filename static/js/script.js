3ce28d804ce99cbf06d8f5a73b525fc298e47c93
Mamoudou ndiaye <alpapie0908@gmail.com>
2023-07-06 17:09:50 +0000

    visualization update

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,12 @@ closebtn=document.getElementById("closebtn")
 header=document.getElementById("after-loading")
 mainpage=document.getElementById("after-loading1")
 loader=document.getElementById("loader")
+// locations=document.getElementsByClassName("fomat-location")
+
+// locations.forEach(location => {
+//     var content=location.value
+//     location.Inne = content.split("-").join(" ").split("").join(" ");
+// });
 
 
 setTimeout(()=>{
@@ -16,7 +22,7 @@ setTimeout(()=>{
 
     header.style.display="block"
     header.style.visibility="visible"
-},4000)
+},2000)
 
 mobilebtn.addEventListener("click",()=>{
     mobileMenu.style.display="block"
@@ -33,3 +39,17 @@ closebtn.addEventListener("click",()=>{
     // mobileMenu.style.display="hidden"
     mobileMenu.style.marginLeft="-100%"
 })
+
+
+locs=document.getElementsByClassName("fomat-location")
+
+// locations.forEach(location => {
+//     var content=location.value
+//     location.Inne = content.split("-").join(" ").split("").join(" ");
+// });
+for (var i = 0; i < locs.length; i++) {
+    loc=locs[i]
+    val=loc.innerHTML
+    loc.innerHTML=val.split("-").join(" ").split("_").join(" ");
+    console.log(loc.innerHTML)
+}
