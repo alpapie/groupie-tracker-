46343595a74605f127ac39079632c4870854a3fb
alpapie <62723349+alpapie@users.noreply.github.com>
2024-03-01 00:27:49 +0000

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -10,31 +9,28 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", handler)
 	PORT := envPortOr("3000")
-	args := os.Args[1:]
-	if len(args) > 0 {
-		tab := strings.Split(args[0], "=")
-		if len(tab) > 1 && helper.IsInt(tab[1]) && tab[1] != "" {
-			t, _ := strconv.Atoi(tab[1])
-			if t >= 1024 && t <= 65535 {
-				PORT = ":" + tab[1]
-			}
-		}
 	}
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	routes.Route()
-	fmt.Println("Listening in http://localhost" + PORT)
+	
+	fmt.Printf("Starting server on port %s...\n", PORT)
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
 
-	log.Fatal(http.ListenAndServe(PORT, nil))
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func envPortOr(port string) string {
-	// If `PORT` variable in environment exists, return it
 	if envPort := os.Getenv("PORT"); envPort != "" {
 		return ":" + envPort
 	}
-	//  Otherwise, return the value of `port` variable from function argument
 	return ":" + port
 }
